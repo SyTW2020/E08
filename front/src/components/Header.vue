cdba55962d0fe1b01baa65ae3dd813e15d87a39f
hernandgg <alu0101050218@ull.edu.es>
2021-08-09 00:09:43 +0100

    Reeditado Header

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <div class="header">
     <b-navbar toggleable="md" type="dark" variant="dark">
@@ -20,5 +21,103 @@
 <script>
 export default {
   name: "Header",
+  prop: {
+      name: {
+        type: Boolean,
+        default:false,
+        required: true,
+      }
+  },
+  data() {
+    return {
+      /* Logged ?? */
+    };
+  }
 };
 </script>
+-->
+
+<template>
+  <nav>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="px-8 py-3 mx-auto border-1 bg-gray-200 shadow-sm">
+      <div class="flex justify-between items-center">
+        <!-- LOGO -->
+        <div class="mr-20">Logo</div>
+
+        <!-- Search -->
+        <div class="ml-10 w-3/12">
+          <div class="bg-white flex items-center rounded-full shadow-xl ">
+            <input class="ml-4 rounded-l-full w-full py-3 px-full text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search">
+            
+            <div>
+              <button class="bg-blue-500 text-white rounded-full px-2.5 py-2.5 mr-2 hover:bg-blue-400 focus:outline-none  flex items-center justify-center">
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Botones -->
+        <div class="flex space-x-3">
+          <button>Carrito</button>
+          <button class="gradient font-bold text-white py-2 px-3 rounded">Mi cuenta</button>
+        </div>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  prop: {
+      name: {
+        type: Boolean,
+        default:false,
+        required: true,
+      }
+  },
+  data() {
+    return {
+      /* Logged ?? */
+    };
+  }
+}
+</script>
+
+<style>
+  .gradient {
+    position: relative;
+    background-image: linear-gradient(
+      to right,
+      hsl(177, 47%, 39%),
+      hsl(279, 38%, 40%)
+    );
+    z-index: 1;
+  }
+
+  .gradient::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: linear-gradient(
+      to left,
+      hsl(177, 47%, 39%),
+      hsl(279, 38%, 40%)
+    );
+    z-index: -1;
+    transition: opacity 0.5s linear;
+    opacity: 0;
+  }
+
+  .gradient:hover::before {
+    opacity: 1;
+  }
+</style>
+
+         
