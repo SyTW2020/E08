89c56889cde16c11ed8ae3a00e61a3f5b2eb04d2
David Rodriguez <alu0101120645@ull.edu.es>
2021-08-12 10:41:28 +0100

    Arreglando login

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,80 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import log from './login.module'
 
-//import axios from 'axios' ;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    token: null,
-    data: {},
 
-  },
-  mutations: {
-    setToken(state,payload){
-      state.token=payload
-    },
-    setData(state,payload){
-      state.data=payload
-    },
-    setProducts(state,payload){
-      state.products=payload
+  modules: {
+    log
+  }
+})
 
-    }
-  },
-  actions: {
-    async login({commit}, usuario){
-      
-      console.log(commit)
-      console.log(usuario)
-      try {                    //url de postman
-        /*
-       const json = JSON.stringify ({usuario})
-        console.log(json)
-        const res = await axios.post('http://localhost:4000/auth/signin',json)
-        const resDB = await res.json()
-        console.log(resDB.token)
-        
-        
-        commit('setToken', resDB.token)
-        commit('setData', resDB.data)
-        localStorage.setItem('token', resDB.token)
-        localStorage.setItem('data', resDB.data)
-        
-        */
-       const json = JSON.stringify ({usuario})
-       console.log(json)
-       const res = await fetch('http://localhost:4000/auth/signin', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(usuario)
-        })
-        let resDB = await res.json()
-        //console.log(resDB.token)
-        console.log(resDB)
-        
-        
-        commit('setToken', resDB.token)
 
-        localStorage.setItem('token', resDB.token)
-        localStorage.setItem('data', resDB.data)
-        
-
-      } catch (error) {
-        console.log(error)
-      }
-
-    },
-    readToken({ commit }) {
-      if(localStorage.getItem('token')){
-        commit('setToken', localStorage.getItem('token'))
-      }else{
-        commit('setToken', null)
-      }
-    }
-  },
-  modules: {},
-});
---
+++ front/src/store/login.module.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const url = 'http://localhost:4000/auth/signin'
+
+const state = {
+    token: null,
+    data: {},
+}
+
+const actions = {
+    async login({commit},usuario) {
+       
+        const res = await axios.post(url, usuario);
+        console.log(res)
+       
+        commit('setToken', res.data.token)
+        commit('setData', res.data.datos)
+        localStorage.setItem('token', res.data.token)
+        localStorage.setItem('data', res.data.datos)
+    }
+};
+
+const mutations = {
+    setToken(state,payload){
+      state.token=payload
+    },
+    setData(state,payload){
+      state.data=payload
+    },
+    };
+  const getters = {}
+
+export default {state, getters, actions, mutations}
