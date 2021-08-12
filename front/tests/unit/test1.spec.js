57ea45cb7866c95eb8b533685a1cc3087008f058
Nitro1000 <alu0101160276@ull.edu.es>
2021-08-12 23:16:27 +0100

    vue_travis.yml

---
+++ front/tests/unit/test1.spec.js
@@ -0,0 +1,17 @@
+import Login_component from "@/components/Login_component";
+
+
+describe('Prueba 1', ()=>{
+    it('should create', ()=>{
+        usuario:{ 
+            email: 'test';
+            password: 'test'
+        }
+        expect(Login_component.data()).toStrictEqual({
+            "usuario": {
+                "email": "yoquese2@gmail.com", 
+                "password": "potasio123"
+            }
+        });
+    })
+})
