d007308244f8293cc98cc323a01d59829fca42c5
Nitro1000 <alu0101160276@ull.edu.es>
2021-08-16 12:27:54 +0100

    test3

--- front/tests/unit/test2.spec.js
+++ front/tests/unit/test2.spec.js
@@ -1,13 +1,52 @@
-import Login_component from "@/components/Login_component";
+import { shallowMount, mount } from "@vue/test-utils";
+import Login from "@/components/Login_component";
+import Register from "@/components/Register";
 
 
-describe('Prueba 2', ()=>{
-    it('should create', ()=>{
-        expect(Login_component.data()).toStrictEqual({
-            "usuario": {
-                "email": "yoquese2@gmail.com", 
-                "password": "potasio123"
+describe('Prueba 2', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Register, {
+            methods: { registrar: () => { } },
+            data() {
+                return {
+                    form: {
+                        user: "",
+                        email: "",
+                        pass: "",
+                        nombre: "",
+                        apellidos: "",
+                        numero: "",
+                        address: "",
+                        poblacion: "",
+                        codigo: "",
+                        cumple: "",
+                    }
+                };
             }
         });
-    })
+    });
+    it('should create', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+})
+
+describe('Prueba 3', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Login, {
+            methods: { login: () => { } },
+            data() {
+                return {
+                    usuario: {
+                        email: 'yoquese2@gmail.com',
+                        password: 'potasio123'
+                    }
+                }
+            }
+        });
+    });
+    it('should create', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
 })
