2d1254657120018be82615e51fd370b494bb3119
David Rodriguez <alu0101120645@ull.edu.es>
2021-08-17 14:47:54 +0100

    añadiendo cambios

--- front/src/store/productos.module.js
+++ front/src/store/productos.module.js
@@ -4,23 +4,12 @@ const url ='http://localhost:4000/products'
 
 const state = {
     products: [],
-    carrito_numero : 0,
     carrito_products : []
 }
 
 const getters = {
     allProducts : state => state.products,
-    getcarritoProduct : state => {
-        return state.added.map (({id,quantity}) => {
-            const product = state.products.find(p.id === id)
-        
-            return {
-                name: product.name,
-                precio: product.precio,
-                quantity
-              }
-        })
-    }
+   
 }
 
 const actions = {
@@ -33,11 +22,7 @@ const actions = {
         await axios.get(`${url}${producto.id}`);
         commit('get', producto);
     },
-    addToCart({ commit }, product){
-        commit(types.ADD_TO_CART, {
-          id: product.id
-        })
-      }
+    
 
 }
 const mutations = {
