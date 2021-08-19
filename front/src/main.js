129342a63e0f262096bb7140f77349a274cba8b9
Nitro1000 <alu0101160276@ull.edu.es>
2021-08-19 17:58:24 +0100

    formulario

--- front/src/main.js
+++ front/src/main.js
@@ -8,10 +8,12 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./index.css";
+import Vuelidate from 'vuelidate';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
+Vue.use(Vuelidate);
 
 
 
